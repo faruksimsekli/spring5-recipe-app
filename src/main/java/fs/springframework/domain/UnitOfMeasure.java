4c3fcf651a1a94ca5e3d8109e4343f4697ddd07a
faruksimsekli <faruksimsekli.7@gmail.com>
2020-08-14 21:40:30 +0300

    added One to One JPA Relationships

---
+++ src/main/java/fs/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,21 @@
+package fs.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
