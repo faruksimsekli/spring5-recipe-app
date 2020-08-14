cd633dad379ff95f2a7ff7a07cd3339165780fd5
faruksimsekli <faruksimsekli.7@gmail.com>
2020-08-14 21:24:14 +0300

    Initial commit

---
+++ src/main/java/fs/springframework/Spring5RecipeAppApplication.java
@@ -0,0 +1,17 @@
+package fs.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Spring5RecipeAppApplication.class, args);
+
+        System.out.println("faruk simsekli sdfdsf 54545");
+
+    }
+
+}
