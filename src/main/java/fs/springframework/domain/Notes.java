b74c19c7eb90645fcd4ee4d9b5689cf9e77b9d2b
faruksimsekli <faruksimsekli.7@gmail.com>
2020-08-19 17:29:28 +0300

    added some testing

--- src/main/java/fs/springframework/domain/Notes.java
+++ src/main/java/fs/springframework/domain/Notes.java
@@ -1,10 +1,12 @@
 package fs.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
