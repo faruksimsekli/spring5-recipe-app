4e1147f3dc832a1341dc3179468ef7777a1ae16e
faruksimsekli <faruksimsekli.7@gmail.com>
2020-08-20 16:45:48 +0300

    creating command objects adn types conversions

--- src/main/java/fs/springframework/services/RecipeService.java
+++ src/main/java/fs/springframework/services/RecipeService.java
@@ -1,12 +1,18 @@
 package fs.springframework.services;
 
+import fs.springframework.commands.RecipeCommand;
 import fs.springframework.domain.Recipe;
 
 import java.util.Set;
 
+/**
+ * Created by jt on 6/13/17.
+ */
 public interface RecipeService {
 
     Set<Recipe> getRecipes();
 
     Recipe findById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
